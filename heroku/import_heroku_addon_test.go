05a49941d9233a0882f8e29b4d01ab073cab5bab
Will Farrington <wcfarrington@gmail.com>
2017-09-13 18:23:44 -0400

    Fixup import heroku_addon tests

--- heroku/import_heroku_addon_test.go
+++ heroku/import_heroku_addon_test.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 	"testing"
 
-	heroku "github.com/cyberdelia/heroku-go/v3"
 	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
 )
 
 func TestAccHerokuAddon_importBasic(t *testing.T) {
-	var addon heroku.AddOn
 	appName := fmt.Sprintf("tftest-%s", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
@@ -21,34 +18,18 @@ func TestAccHerokuAddon_importBasic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccCheckHerokuAddonConfig_basic(appName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckHerokuAddonExists("heroku_addon.foobar", &addon),
-					testAccCheckHerokuAddonAttributes(&addon, "deployhooks:http"),
-					resource.TestCheckResourceAttr(
-						"heroku_addon.foobar", "config.0.url", "http://google.com"),
-					resource.TestCheckResourceAttr(
-						"heroku_addon.foobar", "app", appName),
-					resource.TestCheckResourceAttr(
-						"heroku_addon.foobar", "plan", "deployhooks:http"),
-				),
 			},
 			{
 				ResourceName:            "heroku_addon.foobar",
 				ImportState:             true,
-				ImportStateCheck:        testAccCheckHerokuAddonImportedAttributes,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"config_vars"},
+				ImportStateVerifyIgnore: []string{"config_vars", "config"},
+			},
+			{
+				Config:             testAccCheckHerokuAddonConfig_basic(appName),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: false,
 			},
 		},
 	})
-}
-
-func testAccCheckHerokuAddonImportedAttributes(state []*terraform.InstanceState) error {
-	for _, s := range state {
-		fmt.Printf("%+v\n", s)
-		if app, ok := s.Attributes["app"]; !ok || app == "" {
-			return fmt.Errorf("No Addon app is set")
-		}
-	}
-	return nil
 }
