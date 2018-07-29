c35ff2a08635d5dd93596aec0dd82aa8a3d1df26
Trent Albright <trent.albright@gmail.com>
2018-07-29 12:01:04 -0400

    Add import test for heroku_space_app_access

---
+++ heroku/import_heroku_space_app_access_test.go
@@ -0,0 +1,35 @@
+package heroku
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccHerokuSpaceAppAccess_importBasic(t *testing.T) {
+	spaceName := fmt.Sprintf("tftest1-%s", acctest.RandString(10))
+	org := getTestSpaceOrganizationName()
+	testUser := getTestUser()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccSkipTestIfOrganizationMissing(t)
+			testAccSkipTestIfUserMissing(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckHerokuSpaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckHerokuSpaceAppAccessConfig_basic(spaceName, org, testUser, []string{"create_apps"}),
+			},
+			{
+				ResourceName:      "heroku_space_app_access.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
