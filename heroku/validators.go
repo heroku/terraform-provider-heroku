be5250a4f47b6a1cf803146554644a4785c6bb94
patrick-vanstee <pvanstee@salesforce.com>
2018-08-06 14:20:54 -0400

    Replace cidr notation validator

--- heroku/validators.go
+++ heroku/validators.go
@@ -2,7 +2,6 @@ package heroku
 
 import (
 	"fmt"
-	"net"
 
 	"github.com/satori/uuid"
 )
@@ -11,24 +10,5 @@ func validateUUID(v interface{}, k string) (ws []string, errors []error) {
 	if _, err := uuid.FromString(v.(string)); err != nil {
 		errors = append(errors, fmt.Errorf("%q is an invalid UUID: %s", k, err))
 	}
-	return
-}
-
-// validateCIDRNetworkAddress ensures that the string value is a valid CIDR that
-// represents a network address - it adds an error otherwise
-func validateCIDRNetworkAddress(v interface{}, k string) (ws []string, errors []error) {
-	value := v.(string)
-	_, ipnet, err := net.ParseCIDR(value)
-	if err != nil {
-		errors = append(errors, fmt.Errorf(
-			"%q must contain a valid CIDR, got error parsing: %s", k, err))
-		return
-	}
-
-	if ipnet == nil || value != ipnet.String() {
-		errors = append(errors, fmt.Errorf(
-			"%q must contain a valid network CIDR, got %q", k, value))
-	}
-
 	return
 }
