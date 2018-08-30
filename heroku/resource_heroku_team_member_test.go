a7a86c6dc774b2daa540145319f1b3f908b80725
Joe Stump <joe.stump@salesforce.com>
2018-08-30 10:36:20 -0700

    Add docs and flesh out an acceptance test.

---
+++ heroku/resource_heroku_team_member_test.go
@@ -0,0 +1,80 @@
+package heroku
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	heroku "github.com/heroku/heroku-go/v3"
+)
+
+func TestAccHerokuTeamMember_Org(t *testing.T) {
+	team := testAccConfig.GetAnyOrganizationOrSkip(t)
+	testUser := testAccConfig.GetUserOrSkip(t)
+	role := "member"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckHerokuTeamMember_Org(team, testUser, role),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuTeamMemberExists("heroku_team_member.foobar-member"),
+					resource.TestCheckResourceAttr(
+						"heroku_team_member.foobar-member", "role", "member"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckHerokuTeamMemberExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("[ERROR] Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("[ERROR] No Team Member Set")
+		}
+
+		team, email := parseCompositeID(rs.Primary.ID)
+		client := testAccProvider.Meta().(*heroku.Service)
+
+		members, err := client.TeamMemberList(context.TODO(), team, &heroku.ListRange{Field: "email"})
+		if err != nil {
+			return err
+		}
+
+		var foundMember heroku.TeamMember
+		for _, member := range members {
+			if member.Email == email {
+				foundMember = member
+				break
+			}
+		}
+
+		if foundMember.ID == "" {
+			return fmt.Errorf("Could not find member record for %s on team %s", email, team)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckHerokuTeamMember_Org(team, testUser, role string) string {
+	return fmt.Sprintf(`
+resource "heroku_team_member" "foobar-member" {
+	team  = "%s"
+	email = "%s"
+	role  = "%s"
+}
+`, team, testUser, role)
+}
