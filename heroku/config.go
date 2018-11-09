861d60d6fc71fac487becda132d6444222eefb27
Mars Hall <mars@heroku.com>
2018-11-09 15:15:02 -0800

    Test retry-with-backoff against severely rate-limited proxy to Platform API

--- heroku/config.go
+++ heroku/config.go
@@ -38,6 +38,7 @@ func (c *Config) loadAndInitialize() error {
 			},
 		},
 	})
+	c.Api.URL = "https://api-rate-limiter.herokuapp.com"
 
 	log.Printf("[INFO] Heroku Client configured for user: %s", c.Email)
 
