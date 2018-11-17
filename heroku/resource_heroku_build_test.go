ae80af9a592012b6824e451faa61baf5490a6916
Mars Hall <mars@heroku.com>
2018-11-16 16:57:53 -0800

    Test Build resource with all options

--- heroku/resource_heroku_build_test.go
+++ heroku/resource_heroku_build_test.go
@@ -47,6 +47,25 @@ func TestAccHerokuBuild_InsecureUrl(t *testing.T) {
 	})
 }
 
+func TestAccHerokuBuild_AllOpts(t *testing.T) {
+	var build heroku.Build
+	randString := acctest.RandString(10)
+	appName := fmt.Sprintf("tftest-%s", randString)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckHerokuBuildConfig_allOpts(appName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuBuildExists("heroku_build.foobar", &build),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckHerokuBuildExists(n string, Build *heroku.Build) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -101,6 +120,26 @@ resource "heroku_build" "foobar" {
     app = "${heroku_app.foobar.name}"
     source = {
       url = "http://github.com/mars/cra-example-app/archive/v2.1.1.tar.gz"
+    }
+}`, appName)
+}
+
+func testAccCheckHerokuBuildConfig_allOpts(appName string) string {
+	// Manually generated `checksum` using `shasum --algorithm 256 v2.1.1.tar.gz`
+	// per Heroku docs https://devcenter.heroku.com/articles/slug-checksums
+
+	return fmt.Sprintf(`resource "heroku_app" "foobar" {
+    name = "%s"
+    region = "us"
+}
+
+resource "heroku_build" "foobar" {
+    app = "${heroku_app.foobar.name}"
+    buildpacks = ["mars/create-react-app"]
+    source = {
+      checksum = "SHA256:b7dfb201c9fa6541b64fd450c5e00641c80d7d1e39134b7c12ce601efbb8642b"
+      url = "https://github.com/mars/cra-example-app/archive/v2.1.1.tar.gz"
+      version = "v2.1.1"
     }
 }`, appName)
 }
