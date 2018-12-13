8a11074659379a0e7dc1b0a7b8f62c606d831a01
Trent Albright <trent.albright@gmail.com>
2018-12-12 23:03:26 -0500

    make provider config init more GO idiomatic

--- heroku/provider.go
+++ heroku/provider.go
@@ -1,22 +1,14 @@
 package heroku
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
-	"net/url"
 	"strings"
 
-	"os"
-	"runtime"
-
-	"github.com/bgentry/go-netrc/netrc"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/hashicorp/terraform/terraform"
 	heroku "github.com/heroku/heroku-go/v3"
-	"github.com/mitchellh/go-homedir"
 )
 
 // Provider returns a terraform.ResourceProvider.
@@ -109,31 +101,18 @@ func Provider() terraform.ResourceProvider {
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	config := Config{}
+	log.Println("[INFO] Initializing Heroku provider")
+	config := NewConfig()
 
-	headers := make(map[string]string)
-	if h := d.Get("headers").(string); h != "" {
-		if err := json.Unmarshal([]byte(h), &headers); err != nil {
-			return nil, err
-		}
-	}
-
-	h := make(http.Header)
-	for k, v := range headers {
-		h.Set(k, v)
-	}
-
-	config.Headers = h
-
-	if url, ok := d.GetOk("url"); ok {
-		config.URL = url.(string)
-	}
-
-	err := readNetrcFile(&config)
-	if err != nil {
+	if err := config.applySchema(d); err != nil {
 		return nil, err
 	}
 
+	if err := config.applyNetrcFile(); err != nil {
+		return nil, err
+	}
+
+	//the provider resource schema takes precedence over Netrc
 	if email, ok := d.GetOk("email"); ok {
 		config.Email = email.(string)
 	}
@@ -142,36 +121,13 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		config.APIKey = apiKey.(string)
 	}
 
-	err = applyDelayConfig(d, &config)
-
-	log.Println("[INFO] Initializing Heroku client")
-	if err := config.loadAndInitialize(); err != nil {
+	if err := config.initializeAPI(); err != nil {
 		return nil, err
 	}
 
-	return &config, nil
-}
+	log.Printf("[DEBUG] Heroku provider initialized: %s\n", config)
 
-func applyDelayConfig(d *schema.ResourceData, config *Config) error {
-	if v, ok := d.GetOk("delays"); ok {
-		vL := v.([]interface{})
-		if len(vL) > 1 {
-			return fmt.Errorf("Provider configuration error: only 1 delays config is permitted")
-		}
-		for _, v := range vL {
-			apiConfig := v.(map[string]interface{})
-			if v, ok := apiConfig["post_app_create_delay"].(int); ok && v != 0 {
-				config.PostAppCreateDelay = int64(v)
-			}
-			if v, ok := apiConfig["post_space_create_delay"].(int); ok && v != 0 {
-				config.PostSpaceCreateDelay = int64(v)
-			}
-			if v, ok := apiConfig["post_domain_create_delay"].(int); ok && v != 0 {
-				config.PostDomainCreateDelay = int64(v)
-			}
-		}
-	}
-	return nil
+	return config, nil
 }
 
 func buildCompositeID(a, b string) string {
@@ -187,60 +143,4 @@ func parseCompositeID(id string) (p1 string, p2 string, err error) {
 		err = fmt.Errorf("error: Import composite ID requires two parts separated by colon, eg x:y")
 	}
 	return
-}
-
-// Credit of this method is from https://github.com/Yelp/terraform-provider-signalform
-func readNetrcFile(config *Config) error {
-	// Get the netrc file path. If path not shown, then fall back to default netrc path value
-	path := os.Getenv("NETRC_PATH")
-
-	if path == "" {
-		filename := ".netrc"
-		if runtime.GOOS == "windows" {
-			filename = "_netrc"
-		}
-
-		var err error
-		path, err = homedir.Expand("~/" + filename)
-		if err != nil {
-			return err
-		}
-	}
-
-	// If the file is not a file, then do nothing
-	if fi, err := os.Stat(path); err != nil {
-		// File doesn't exist, do nothing
-		if os.IsNotExist(err) {
-			return nil
-		}
-
-		// Some other error!
-		return err
-	} else if fi.IsDir() {
-		// File is directory, ignore
-		return nil
-	}
-
-	// Load up the netrc file
-	net, err := netrc.ParseFile(path)
-	if err != nil {
-		return fmt.Errorf("error parsing netrc file at %q: %s", path, err)
-	}
-
-	u, err := url.Parse(config.URL)
-	if err != nil {
-		return err
-	}
-
-	machine := net.FindMachine(u.Host)
-	if machine == nil {
-		// Machine not found, no problem
-		return nil
-	}
-
-	// Set the user/api key/headers
-	config.Email = machine.Login
-	config.APIKey = machine.Password
-
-	return nil
 }
