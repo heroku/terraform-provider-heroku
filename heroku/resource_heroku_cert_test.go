27ccb117498cc5d604e4cf35fffaa4031982f086
Michael Hale <mhale@heroku.com>
2021-06-03 18:06:34 -0400

    format

--- heroku/resource_heroku_cert_test.go
+++ heroku/resource_heroku_cert_test.go
@@ -1,61 +1,61 @@
 package heroku
 
 import (
-  "context"
-  "fmt"
-  "io/ioutil"
-  "os"
-  "strings"
-  "testing"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
 
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-  heroku "github.com/heroku/heroku-go/v5"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	heroku "github.com/heroku/heroku-go/v5"
 )
 
 func TestAccHerokuCert(t *testing.T) {
-  var endpoint heroku.SniEndpoint
-  appName := fmt.Sprintf("tftest-%s", acctest.RandString(10))
+	var endpoint heroku.SniEndpoint
+	appName := fmt.Sprintf("tftest-%s", acctest.RandString(10))
 
-  wd, _ := os.Getwd()
-  certFile := wd + "/test-fixtures/terraform.cert"
-  certFile2 := wd + "/test-fixtures/terraform2.cert"
-  keyFile := wd + "/test-fixtures/terraform.key"
-  keyFile2 := wd + "/test-fixtures/terraform2.key"
+	wd, _ := os.Getwd()
+	certFile := wd + "/test-fixtures/terraform.cert"
+	certFile2 := wd + "/test-fixtures/terraform2.cert"
+	keyFile := wd + "/test-fixtures/terraform.key"
+	keyFile2 := wd + "/test-fixtures/terraform2.key"
 
-  certificateChainBytes, _ := ioutil.ReadFile(certFile)
-  certificateChain := string(certificateChainBytes)
-  certificateChain2Bytes, _ := ioutil.ReadFile(certFile2)
-  certificateChain2 := string(certificateChain2Bytes)
+	certificateChainBytes, _ := ioutil.ReadFile(certFile)
+	certificateChain := string(certificateChainBytes)
+	certificateChain2Bytes, _ := ioutil.ReadFile(certFile2)
+	certificateChain2 := string(certificateChain2Bytes)
 
-  slugID := testAccConfig.GetSlugIDOrSkip(t)
+	slugID := testAccConfig.GetSlugIDOrSkip(t)
 
-  resource.Test(t, resource.TestCase{
-    PreCheck:     func() { testAccPreCheck(t) },
-    Providers:    testAccProviders,
-    CheckDestroy: testAccCheckHerokuCertDestroy,
-    Steps: []resource.TestStep{
-      {
-        Config: testAccCheckHerokuCertConfig(appName, slugID, certFile2, keyFile2),
-        Check: resource.ComposeTestCheckFunc(
-          testAccCheckHerokuCertExists("heroku_cert.ssl_certificate", &endpoint),
-          testAccCheckHerokuCertificateChain(&endpoint, certificateChain2),
-        ),
-      },
-      {
-        Config: testAccCheckHerokuCertConfig(appName, slugID, certFile, keyFile),
-        Check: resource.ComposeTestCheckFunc(
-          testAccCheckHerokuCertExists("heroku_cert.ssl_certificate", &endpoint),
-          testAccCheckHerokuCertificateChain(&endpoint, certificateChain),
-        ),
-      },
-    },
-  })
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckHerokuCertDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckHerokuCertConfig(appName, slugID, certFile2, keyFile2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuCertExists("heroku_cert.ssl_certificate", &endpoint),
+					testAccCheckHerokuCertificateChain(&endpoint, certificateChain2),
+				),
+			},
+			{
+				Config: testAccCheckHerokuCertConfig(appName, slugID, certFile, keyFile),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuCertExists("heroku_cert.ssl_certificate", &endpoint),
+					testAccCheckHerokuCertificateChain(&endpoint, certificateChain),
+				),
+			},
+		},
+	})
 }
 
 func testAccCheckHerokuCertConfig(appName, slugID, certFile, keyFile string) string {
-  return strings.TrimSpace(fmt.Sprintf(`
+	return strings.TrimSpace(fmt.Sprintf(`
 resource "heroku_app" "foobar" {
   name = "%s"
   region = "us"
@@ -86,60 +86,60 @@ resource "heroku_cert" "ssl_certificate" {
 }
 
 func testAccCheckHerokuCertDestroy(s *terraform.State) error {
-  client := testAccProvider.Meta().(*Config).Api
+	client := testAccProvider.Meta().(*Config).Api
 
-  for _, rs := range s.RootModule().Resources {
-    if rs.Type != "heroku_cert" {
-      continue
-    }
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "heroku_cert" {
+			continue
+		}
 
-    _, err := client.SniEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
+		_, err := client.SniEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
 
-    if err == nil {
-      return fmt.Errorf("Cerfificate still exists")
-    }
-  }
+		if err == nil {
+			return fmt.Errorf("Cerfificate still exists")
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func testAccCheckHerokuCertificateChain(endpoint *heroku.SniEndpoint, chain string) resource.TestCheckFunc {
-  return func(s *terraform.State) error {
+	return func(s *terraform.State) error {
 
-    if endpoint.CertificateChain != chain {
-      return fmt.Errorf("Bad certificate chain: %s", endpoint.CertificateChain)
-    }
+		if endpoint.CertificateChain != chain {
+			return fmt.Errorf("Bad certificate chain: %s", endpoint.CertificateChain)
+		}
 
-    return nil
-  }
+		return nil
+	}
 }
 
 func testAccCheckHerokuCertExists(n string, endpoint *heroku.SniEndpoint) resource.TestCheckFunc {
-  return func(s *terraform.State) error {
-    rs, ok := s.RootModule().Resources[n]
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
 
-    if !ok {
-      return fmt.Errorf("Not found: %s", n)
-    }
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
 
-    if rs.Primary.ID == "" {
-      return fmt.Errorf("No SNI endpoint ID is set")
-    }
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No SNI endpoint ID is set")
+		}
 
-    client := testAccProvider.Meta().(*Config).Api
+		client := testAccProvider.Meta().(*Config).Api
 
-    foundEndpoint, err := client.SniEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
+		foundEndpoint, err := client.SniEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
 
-    if err != nil {
-      return err
-    }
+		if err != nil {
+			return err
+		}
 
-    if foundEndpoint.ID != rs.Primary.ID {
-      return fmt.Errorf("SNI endpoint not found")
-    }
+		if foundEndpoint.ID != rs.Primary.ID {
+			return fmt.Errorf("SNI endpoint not found")
+		}
 
-    *endpoint = *foundEndpoint
+		*endpoint = *foundEndpoint
 
-    return nil
-  }
+		return nil
+	}
 }
