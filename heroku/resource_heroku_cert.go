338e81b036f2e1a0c5b21cc8e4b5aabf3ebeaca0
Michael Hale <mhale@heroku.com>
2021-06-15 16:46:29 -0400

    support legacy ssl:endpoint configurations

--- heroku/resource_heroku_cert.go
+++ heroku/resource_heroku_cert.go
@@ -4,10 +4,50 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/url"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	heroku "github.com/heroku/heroku-go/v5"
+	v5 "github.com/heroku/heroku-go/v5"
 )
+
+type Endpoint interface {
+	Name() string
+	CertificateChain() string
+	CName() string
+}
+
+type SniEndpoint struct {
+	herokuEndpoint *heroku.SniEndpoint
+}
+
+type SSLEndpoint struct {
+	herokuEndpoint *heroku.SSLEndpoint
+}
+
+func (e SniEndpoint) Name() string {
+	return e.herokuEndpoint.Name
+}
+
+func (e SniEndpoint) CName() string {
+	return ""
+}
+
+func (e SniEndpoint) CertificateChain() string {
+	return e.herokuEndpoint.CertificateChain
+}
+
+func (e SSLEndpoint) Name() string {
+	return e.herokuEndpoint.Name
+}
+
+func (e SSLEndpoint) CName() string {
+	return e.herokuEndpoint.CName
+}
+
+func (e SSLEndpoint) CertificateChain() string {
+	return e.herokuEndpoint.CertificateChain
+}
 
 func resourceHerokuCert() *schema.Resource {
 	return &schema.Resource{
@@ -47,8 +87,56 @@ func resourceHerokuCert() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+
+			"legacy": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 	}
+}
+
+func resourceHerokuLegacyCertImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	client := meta.(*Config).Api
+
+	app, id, err := parseCompositeID(d.Id())
+	if err != nil {
+		return nil, err
+	}
+
+	ep, err := client.SSLEndpointInfo(context.Background(), app, id)
+	if err != nil {
+		return nil, err
+	}
+
+	d.SetId(ep.ID)
+	d.Set("legacy", true)
+	setErr := d.Set("app", app)
+	if setErr != nil {
+		return nil, setErr
+	}
+
+	return []*schema.ResourceData{d}, nil
+}
+
+func errToHttpError(err error) *v5.Error {
+	urlErr, ok := err.(*url.Error)
+	if ok {
+		v5Err, ok := urlErr.Err.(v5.Error)
+		if ok {
+			return &v5Err
+		}
+	}
+
+	return nil
+}
+
+func isHttpStatusCode(err error, statusCode int) bool {
+	if httpErr := errToHttpError(err); httpErr != nil {
+		return httpErr.StatusCode == statusCode
+	}
+
+	return false
 }
 
 func resourceHerokuCertImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
@@ -61,7 +149,11 @@ func resourceHerokuCertImport(d *schema.ResourceData, meta interface{}) ([]*sche
 
 	ep, err := client.SniEndpointInfo(context.Background(), app, id)
 	if err != nil {
-		return nil, err
+		if isHttpStatusCode(err, 404) {
+			return resourceHerokuLegacyCertImport(d, meta)
+		} else {
+			return nil, err
+		}
 	}
 
 	d.SetId(ep.ID)
@@ -73,13 +165,62 @@ func resourceHerokuCertImport(d *schema.ResourceData, meta interface{}) ([]*sche
 	return []*schema.ResourceData{d}, nil
 }
 
-func resourceHerokuCertCreate(d *schema.ResourceData, meta interface{}) error {
+func hasLegacySSLEndpoint(client *heroku.Service, appIdentity string) (bool, error) {
+	addOns, err := client.AddOnListByApp(context.TODO(), appIdentity, &heroku.ListRange{Field: "id"})
+	if err != nil {
+		return false, fmt.Errorf("Error looking up add-ons: %s", err)
+	}
+
+	for _, a := range addOns {
+		if a.Plan.Name == "ssl:endpoint" {
+			// TODO list endpoints?
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func resourceHerokuLegacyCertCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Config).Api
 
 	app := d.Get("app").(string)
+	preprocess := true
+	opts := heroku.SSLEndpointCreateOpts{
+		CertificateChain: d.Get("certificate_chain").(string),
+		Preprocess:       &preprocess,
+		PrivateKey:       d.Get("private_key").(string),
+	}
+
+	log.Printf("[DEBUG] SSL Certificate create configuration: %#v, %#v", app, opts)
+	a, err := client.SSLEndpointCreate(context.TODO(), app, opts)
+	if err != nil {
+		return fmt.Errorf("Error creating SSL endpoint: %s", err)
+	}
+
+	d.SetId(a.ID)
+	d.Set("legacy", true)
+	log.Printf("[INFO] SSL Certificate ID: %s", d.Id())
+
+	return resourceHerokuCertRead(d, meta)
+}
+
+func resourceHerokuCertCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Config).Api
+	app := d.Get("app").(string)
+
+	hasLegacySSL, err := hasLegacySSLEndpoint(client, app)
+	if err != nil {
+		return err
+	}
+	if hasLegacySSL {
+		return resourceHerokuLegacyCertCreate(d, meta)
+	}
+
 	opts := heroku.SniEndpointCreateOpts{
 		CertificateChain: d.Get("certificate_chain").(string),
-		PrivateKey:       d.Get("private_key").(string)}
+		PrivateKey:       d.Get("private_key").(string),
+	}
 
 	log.Printf("[DEBUG] SSL Certificate create configuration: %#v, %#v", app, opts)
 	a, err := client.SniEndpointCreate(context.TODO(), app, opts)
@@ -95,33 +236,52 @@ func resourceHerokuCertCreate(d *schema.ResourceData, meta interface{}) error {
 
 func resourceHerokuCertRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Config).Api
+	legacy := d.Get("legacy").(bool)
 
-	cert, err := resourceHerokuSSLCertRetrieve(
-		d.Get("app").(string), d.Id(), client)
+	cert, err := resourceHerokuSSLCertRetrieve(legacy, d.Get("app").(string), d.Id(), client)
 	if err != nil {
 		return err
 	}
 
-	d.Set("certificate_chain", cert.CertificateChain)
-	d.Set("name", cert.Name)
+	d.Set("certificate_chain", cert.CertificateChain())
+	d.Set("name", cert.Name())
+	d.Set("cname", cert.CName())
 
 	return nil
 }
 
 func resourceHerokuCertUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Config).Api
+	fmt.Printf("%#v", d.Get("legacy"))
+	legacy := d.Get("legacy").(bool)
 
 	app := d.Get("app").(string)
-	opts := heroku.SniEndpointUpdateOpts{
-		CertificateChain: d.Get("certificate_chain").(string),
-		PrivateKey:       d.Get("private_key").(string),
-	}
 
 	if d.HasChange("certificate_chain") || d.HasChange("private_key") {
-		log.Printf("[DEBUG] SSL Certificate update configuration: %#v, %#v", app, opts)
-		_, err := client.SniEndpointUpdate(context.TODO(), app, d.Id(), opts)
-		if err != nil {
-			return fmt.Errorf("Error updating Sni endpoint: %s", err)
+		if legacy {
+			preprocess := true
+			opts := heroku.SSLEndpointUpdateOpts{
+				CertificateChain: heroku.String(d.Get("certificate_chain").(string)),
+				Preprocess:       &preprocess,
+				PrivateKey:       heroku.String(d.Get("private_key").(string)),
+			}
+
+			log.Printf("[DEBUG] SSL Certificate update configuration: %#v, %#v", app, opts)
+			_, err := client.SSLEndpointUpdate(context.TODO(), app, d.Id(), opts)
+			if err != nil {
+				return fmt.Errorf("Error updating SSL endpoint: %s", err)
+			}
+		} else {
+			opts := heroku.SniEndpointUpdateOpts{
+				CertificateChain: d.Get("certificate_chain").(string),
+				PrivateKey:       d.Get("private_key").(string),
+			}
+
+			log.Printf("[DEBUG] SSL Certificate update configuration: %#v, %#v", app, opts)
+			_, err := client.SniEndpointUpdate(context.TODO(), app, d.Id(), opts)
+			if err != nil {
+				return fmt.Errorf("Error updating Sni endpoint: %s", err)
+			}
 		}
 	}
 
@@ -130,25 +290,40 @@ func resourceHerokuCertUpdate(d *schema.ResourceData, meta interface{}) error {
 
 func resourceHerokuCertDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Config).Api
+	legacy := d.Get("legacy").(bool)
 
 	log.Printf("[INFO] Deleting SSL Cert: %s", d.Id())
 
-	// Destroy the endpoint
-	_, err := client.SniEndpointDelete(context.TODO(), d.Get("app").(string), d.Id())
-	if err != nil {
-		return fmt.Errorf("Error deleting SSL Cert: %s", err)
+	if legacy {
+		_, err := client.SSLEndpointDelete(context.TODO(), d.Get("app").(string), d.Id())
+		if err != nil {
+			return fmt.Errorf("Error deleting SSL Cert: %s", err)
+		}
+	} else {
+		_, err := client.SniEndpointDelete(context.TODO(), d.Get("app").(string), d.Id())
+		if err != nil {
+			return fmt.Errorf("Error deleting SSL Cert: %s", err)
+		}
 	}
 
 	d.SetId("")
 	return nil
 }
 
-func resourceHerokuSSLCertRetrieve(app string, id string, client *heroku.Service) (*heroku.SniEndpoint, error) {
+func resourceHerokuSSLCertRetrieve(legacy bool, app string, id string, client *heroku.Service) (Endpoint, error) {
+	if legacy {
+		endpoint, err := client.SSLEndpointInfo(context.TODO(), app, id)
+		if err != nil {
+			return nil, fmt.Errorf("Error retrieving SSL Cert: %s", err)
+		}
+
+		return &SSLEndpoint{endpoint}, nil
+	}
 	endpoint, err := client.SniEndpointInfo(context.TODO(), app, id)
 
 	if err != nil {
 		return nil, fmt.Errorf("Error retrieving SSL Cert: %s", err)
 	}
 
-	return endpoint, nil
+	return &SniEndpoint{endpoint}, nil
 }
--- heroku/resource_heroku_cert_test.go
+++ heroku/resource_heroku_cert_test.go
@@ -5,14 +5,61 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	heroku "github.com/heroku/heroku-go/v5"
 )
+
+func TestAccHerokuCert_Legacy(t *testing.T) {
+	var endpoint heroku.SSLEndpoint
+	appName := fmt.Sprintf("tftest-%s", acctest.RandString(10))
+
+	wd, _ := os.Getwd()
+	certFile := wd + "/test-fixtures/terraform.cert"
+	certFile2 := wd + "/test-fixtures/terraform2.cert"
+	keyFile := wd + "/test-fixtures/terraform.key"
+	keyFile2 := wd + "/test-fixtures/terraform2.key"
+
+	certificateChainBytes, _ := ioutil.ReadFile(certFile)
+	certificateChain := string(certificateChainBytes)
+	certificateChain2Bytes, _ := ioutil.ReadFile(certFile2)
+	certificateChain2 := string(certificateChain2Bytes)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckHerokuCertDestroyLegacy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckHerokuCertLegacyConfig(appName, certFile2, keyFile2),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuLegacyCertExists("heroku_cert.ssl_certificate", &endpoint),
+					testAccCheckHerokuLegacyCertificateChain(&endpoint, certificateChain2),
+					resource.TestMatchResourceAttr(
+						"heroku_cert.ssl_certificate",
+						"cname", regexp.MustCompile(`^[^\.]+.ssl.herokudns.com$`)),
+				),
+			},
+			{
+				PreConfig: sleep(t, 15),
+				Config:    testAccCheckHerokuCertLegacyConfig(appName, certFile, keyFile),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHerokuLegacyCertExists("heroku_cert.ssl_certificate", &endpoint),
+					testAccCheckHerokuLegacyCertificateChain(&endpoint, certificateChain),
+					resource.TestMatchResourceAttr(
+						"heroku_cert.ssl_certificate",
+						"cname", regexp.MustCompile(`^[^\.]+.ssl.herokudns.com$`)),
+				),
+			},
+		},
+	})
+}
 
 func TestAccHerokuCert(t *testing.T) {
 	var endpoint heroku.SniEndpoint
@@ -42,7 +89,8 @@ func TestAccHerokuCert(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccCheckHerokuCertConfig(appName, certFile, keyFile),
+				PreConfig: sleep(t, 15),
+				Config:    testAccCheckHerokuCertConfig(appName, certFile, keyFile),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckHerokuCertExists("heroku_cert.ssl_certificate", &endpoint),
 					testAccCheckHerokuCertificateChain(&endpoint, certificateChain),
@@ -106,6 +154,66 @@ func testAccCheckHerokuCertDestroy(s *terraform.State) error {
 	return nil
 }
 
+func sleep(t *testing.T, amount time.Duration) func() {
+	return func() {
+		time.Sleep(amount * time.Second)
+	}
+}
+
+func testAccCheckHerokuCertLegacyConfig(appName, certFile, keyFile string) string {
+	return strings.TrimSpace(fmt.Sprintf(`
+resource "heroku_app" "foobar" {
+  name = "%s"
+  region = "us"
+}
+resource "heroku_addon" "ssl" {
+  app = "${heroku_app.foobar.name}"
+  plan = "ssl:endpoint"
+}
+resource "heroku_cert" "ssl_certificate" {
+  app = "${heroku_app.foobar.name}"
+  depends_on = ["heroku_addon.ssl"]
+  certificate_chain="${file("%s")}"
+  private_key="${file("%s")}"
+}`, appName, certFile, keyFile))
+}
+
+func createSslEndpoint(app, cert, key string) error {
+	client := testAccProvider.Meta().(*Config).Api
+
+	preprocess := true
+	opts := heroku.SSLEndpointCreateOpts{
+		CertificateChain: cert,
+		Preprocess:       &preprocess,
+		PrivateKey:       key,
+	}
+	_, err := client.SSLEndpointCreate(context.TODO(), app, opts)
+
+	if err == nil {
+		return fmt.Errorf("Cerfificate still exists")
+	}
+
+	return nil
+}
+
+func testAccCheckHerokuCertDestroyLegacy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*Config).Api
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "heroku_cert" {
+			continue
+		}
+
+		_, err := client.SSLEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
+
+		if err == nil {
+			return fmt.Errorf("Cerfificate still exists")
+		}
+	}
+
+	return nil
+}
+
 func testAccCheckHerokuCertificateChain(endpoint *heroku.SniEndpoint, chain string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
@@ -139,6 +247,47 @@ func testAccCheckHerokuCertExists(n string, endpoint *heroku.SniEndpoint) resour
 
 		if foundEndpoint.ID != rs.Primary.ID {
 			return fmt.Errorf("SNI endpoint not found")
+		}
+
+		*endpoint = *foundEndpoint
+
+		return nil
+	}
+}
+
+func testAccCheckHerokuLegacyCertificateChain(endpoint *heroku.SSLEndpoint, chain string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		if endpoint.CertificateChain != chain {
+			return fmt.Errorf("Bad certificate chain: %s", endpoint.CertificateChain)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckHerokuLegacyCertExists(n string, endpoint *heroku.SSLEndpoint) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No SSL endpoint ID is set")
+		}
+
+		client := testAccProvider.Meta().(*Config).Api
+
+		foundEndpoint, err := client.SSLEndpointInfo(context.TODO(), rs.Primary.Attributes["app"], rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if foundEndpoint.ID != rs.Primary.ID {
+			return fmt.Errorf("SSL endpoint not found")
 		}
 
 		*endpoint = *foundEndpoint
